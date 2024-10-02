d6af153bb91a5e91f24ef4c3b138a14bef26b93b
Aleksandr0917 <180825344+Aleksandr0917@users.noreply.github.com>
2024-10-02 08:54:30 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,9 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+my_list = len([42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5])
+print(my_list)
+while my_list > 0:
+    my_list = int(input('Введите число: '))
+    if my_list < 0:
+
+        print('нет числа')
+

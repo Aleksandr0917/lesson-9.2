e8a9523c1a7472b5e6d68c69fb2c4cefe2e15c61
Aleksandr0917 <180825344+Aleksandr0917@users.noreply.github.com>
2024-10-02 08:56:11 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,9 +1,11 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-my_list = len([42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5])
-print(my_list)
-while my_list > 0:
-    my_list = int(input('Введите число: '))
-    if my_list < 0:
-
-        print('нет числа')
+a = 0
+while a < len(my_list):
+    if my_list[a] >= 0:
+        if my_list[a] != 0:
+            print(my_list[a])
+        a = a + 1
+        continue
+    else:
+        break
 
